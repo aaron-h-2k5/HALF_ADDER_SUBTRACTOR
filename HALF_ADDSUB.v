11ab7281cba264fcf7f1dd9d32da209740437dc2
aaron-h-2k5 <144250957+aaron-h-2k5@users.noreply.github.com>
2024-03-25 08:30:24 +0530

    Update HALF_ADDSUB.v

--- HALF_ADDSUB.v
+++ HALF_ADDSUB.v
@@ -1,7 +1,10 @@
 module HALF_ADDSUB(a,b,sum,carry,D,Bo);
 input a,b;
 output sum,carry,D,Bo; // Outputs sum and carry for half adder:Outputs difference D,Borrow Bo for half subtractor
-//TYPE HERE THE COMMAND FOR SUM GENERATION IN GATE LEVEL MODELLING
-//TYPE HERE THE COMMAND FOR CARRY GENERATION IN GATE LEVEL MODELLING
-//Type logic for half subtractor difference D,Borrow Bo using gate level modelling
+xor (sum,a,b);
+and (carry,a,b);
+wire acomp;
+not (acomp,a);
+xor (D,a,b);
+and(Bo,acomp,b);
 endmodule
