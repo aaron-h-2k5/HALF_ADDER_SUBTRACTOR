a6e737dc8839151557db30a9e2c71110f6eb269e
aaron-h-2k5 <144250957+aaron-h-2k5@users.noreply.github.com>
2024-03-25 08:32:39 +0530

    Update HALF_ADDSUB.v

--- HALF_ADDSUB.v
+++ HALF_ADDSUB.v
@@ -1,8 +1,10 @@
 module HALF_ADDSUB(a,b,sum,carry,D,Bo);
 input a,b;
 output sum,carry,D,Bo; // Outputs sum and carry for half adder:Outputs difference D,Borrow Bo for half subtractor
+// Half Adder
 xor (sum,a,b);
 and (carry,a,b);
+// Half Subtractor
 wire acomp;
 not (acomp,a);
 xor (D,a,b);
